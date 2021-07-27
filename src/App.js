2e2f086b57a3acdb3533158c9dce7c1c37d03fcc
Yves Ganza <ganzayvana@gmail.com>
2021-07-27 06:40:57 +0200

    Save First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios';
 
 function App() {
+  const [email, Setemail]=useState("");
+ 
+
+
+
+  const HandleSubmit=(ev)=>{
+    ev.preventDefault();
+      axios.post('https://yvessubscriber.herokuapp.com/create',{email:email})
+      .then(()=>{
+
+        Setemail("");
+        console.log("Success");
+      })
+      
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className="Titles">
+        <h3> <sapn className="Logo">/ /</sapn>Yves Ganza</h3>
+        <span>Subscribe to our blog and get the latest update straight to your Inbox</span>
+        <h4> MailChip Api</h4>
+      </div>
+      <form onSubmit={HandleSubmit} className="Forms">
+        <input type="text" placeholder="Your Email" value={email} onChange={(ev)=>Setemail(ev.target.value)}></input>
+        <button>Subscribe</button>
+      </form>
+      <div className="results">
+      </div>
     </div>
   );
 }
